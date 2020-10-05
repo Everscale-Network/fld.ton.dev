f3470e90d2ee21b7ced029077640c095371ec700
SVT <svt@zmail.ru>
2020-10-05 22:31:55 +0300

    add disclamers

--- scripts/freebsd-build.sh
+++ scripts/freebsd-build.sh
@@ -1,4 +1,23 @@
-#!/bin/bash -eE
+#!/bin/bash 
+
+# (C) Sergey Tyurin  2020-09-05 15:00:00
+
+# Disclaimer
+##################################################################################################################
+# You running this script/function means you will not blame the author(s)
+# if this breaks your stuff. This script/function is provided AS IS without warranty of any kind. 
+# Author(s) disclaim all implied warranties including, without limitation, 
+# any implied warranties of merchantability or of fitness for a particular purpose. 
+# The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
+# In no event shall author(s) be held liable for any damages whatsoever 
+# (including, without limitation, damages for loss of business profits, business interruption, 
+# loss of business information, or other pecuniary loss) arising out of the use of or inability 
+# to use the script or documentation. Neither this script/function, 
+# nor any part of it other than those parts that are explicitly copied from others, 
+# may be republished without author(s) express written permission. 
+# Author(s) retain the right to alter this disclaimer at any time.
+##################################################################################################################
+ 
 
 function pause(){
  read -s -n 1 -p "Press any key to continue . . ."
--- scripts/transfer_amount.sh
+++ scripts/transfer_amount.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+# (C) Sergey Tyurin  2020-09-05 15:00:00
 
+# Disclaimer
+##################################################################################################################
+# You running this script/function means you will not blame the author(s)
+# if this breaks your stuff. This script/function is provided AS IS without warranty of any kind. 
+# Author(s) disclaim all implied warranties including, without limitation, 
+# any implied warranties of merchantability or of fitness for a particular purpose. 
+# The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
+# In no event shall author(s) be held liable for any damages whatsoever 
+# (including, without limitation, damages for loss of business profits, business interruption, 
+# loss of business information, or other pecuniary loss) arising out of the use of or inability 
+# to use the script or documentation. Neither this script/function, 
+# nor any part of it other than those parts that are explicitly copied from others, 
+# may be republished without author(s) express written permission. 
+# Author(s) retain the right to alter this disclaimer at any time.
+##################################################################################################################
+ 
 function tr_usage(){
 echo
 echo " use: transfer_amount.sh <SRC> <DST> <AMOUNT> [new]"
