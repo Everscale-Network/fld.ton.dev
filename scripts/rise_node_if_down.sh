6541b82b57f969571bf35a13ef515b0a6b6b17fa
Custler <svt@zmail.ru>
2020-12-25 19:53:16 +0300

    add script rise_node_if_down.sh

---
+++ scripts/rise_node_if_down.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eE
+
+
+VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
+# echo "Engine PID: $VAL_PID"
+
+if [[ ! -z $VAL_PID ]]; then
+    exit 0
+fi
+
+SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
+# shellcheck source=env.sh
+. "${SCRIPT_DIR}/env.sh"
+
+verb="${1:-1}"
+
+# $SCRIPT_DIR/nets_config_update.sh
+echo "############## - !!! Restart NODE !!! - ##############"
+echo "$(date)"
+./run.sh $verb
+
+VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
+if [[ -z $VAL_PID ]]; then
+  while true
+  do
+    ./run.sh $verb
+    VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
+    [[ ! -z $VAL_PID ]] && break
+    echo "### - ALARM !!! Can't start engine."
+    ${SCRIPT_DIR}/Send_msg_toTelBot.sh "$HOSTNAME Server" "### - ALARM !!! Can't start engine." 2>&1 > /dev/null
+  done
+fi
+echo "=================================================================================================="
+
