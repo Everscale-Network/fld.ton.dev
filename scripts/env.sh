6469e4e9420373bc8fe7eb5a5b6c21dbfea690b9
Custler <svt@zmail.ru>
2021-03-13 16:21:47 +0300

    update FLD_NET_ID

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,7 +5,7 @@ OS_SYSTEM=`uname -s`
 export NETWORK_TYPE="fld.ton.dev"
 export MAIN_NET_ID="58FFCA1A178DAFF7"
 export DEV_NET_ID="A8069625AC5BF68F"
-export FLD_NET_ID="1BA9AB32ADCE75B3"
+export FLD_NET_ID="96717B78DF5ABF6A"
 
 NetName="${NETWORK_TYPE%%.*}"
 case "$NetName" in
